8c994519d694d3c8bc6417b1985e39fa4d14ac11
Cường Lê <cuongle@Cuongs-Mac-Pro.local>
2024-10-28 21:19:01 +0700

    N-2: Connect DB Mongo

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
-interface Local {
-  name: string;
-  age: number;
-}
-
-interface Local1 extends Local {
-  city: string;
-}
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const local: Local1 = {
-    name: 'John Doe',
-    age: 30,
-    city: 'New York',
-  };
-  const fn = (a: number, b: number): number => a + b;
-
-  fn(1, 2);
-
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get<string>('PORT') ?? 3000);
 }
 bootstrap();
